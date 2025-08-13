5ff7d8ae2730ea9f4a6b0dee17cba3b97d6ab080
Oleg-angren <159626758+Oleg-angren@users.noreply.github.com>
2025-08-13 21:18:33 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -34,8 +34,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-#if __name__ == "__main__":
- #   asyncio.run(main())
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    web.run_app(app, host="0.0.0.0", port=port)
+   asyncio.run(main())
+
